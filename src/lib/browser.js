2bb2a6a23d418e3f854cfa68a27fd050e2353413
Dmitriy Mozgovoy <robotshara@gmail.com>
2018-12-02 19:26:11 +0200

    fixed parseURL function, refactored onDOMReady

--- src/lib/browser.js
+++ src/lib/browser.js
@@ -1,14 +1,25 @@
-
-export const parseURL= typeof URL!='undefined'? (url)=>new URL(url, window.location.href) : (url)=>{
+export const parseURL = typeof URL != 'undefined' ? (url) => new URL(url, window.location.href) : (url) => {
     let a = document.createElement("a");
-    a.href = url;
-    return Object.assign({}, a);
+
+    return {
+        href: (a.href = url),
+        protocol: a.protocol,
+        host: a.host,
+        hostname: a.hostname,
+        port: a.port,
+        pathname: a.pathname,
+        hash: a.hash,
+        search: a.search,
+        origin: a.origin
+    }
 };
 
-export function onDOMReady(callback){
-    if(~['interactive','complete'].indexOf(document.readyState)){
+export function onDOMReady(callback) {
+    let s = document.readyState;
+    
+    if (s == 'interactive' || s == 'complete') {
         callback();
-    }else{
+    } else {
         document.addEventListener("DOMContentLoaded", function handler() {
             document.removeEventListener("DOMContentLoaded", handler);
             callback()
