dbab516e6bf58ba556555982573d0609a1b06d3c
Dmitriy Mozgovoy <robotshara@gmail.com>
2018-12-07 01:18:34 +0200

    simplified randomStr

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,38 +1,16 @@
 export let global = new Function('return this')();
 
-export const chars= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
 const {toString}= Object.prototype;
 
 
-export function randomStr(length = 6, pool = chars) {
+export function randomStr(length = 6) {
     let str = "";
 
     while (length-- > 0) {
-        str += pool[(Math.floor(pool.length * Math.random()))];
+        str += Math.floor(36 * Math.random()).toString(36)
     }
 
     return str;
-}
-
-export function dropElement(arr, value, rtl) {
-    const index = arr[rtl ? 'indexOf' : 'lastIndexOf'](value);
-    if (~index){
-        arr.splice(index, 1);
-    }
-
-    return !!~index;
-}
-
-export function promisify(fn){
-    return new function(...args){
-        return new Promise((resolve, reject)=>{
-            fn.apply(this, args.push((err, ...data)=>{
-                err? reject(err) : resolve(data.length>1? data : data[0]);
-            }))
-        })
-    };
-
 }
 
 export const generateUniquePropName= (obj, generator= (i)=>`${i.toString(36)}`, maxIterations= 1000)=>{
@@ -91,17 +69,3 @@ export const encodeParams= (params) =>params ? Object.keys(params).map(param => 
     let rawValue = params[param];
     return `${param}=${encodeURIComponent(rawValue && typeof rawValue == 'object'? JSON.stringify(rawValue) : ('' + rawValue))}`;
 }).join('&') : '';
-
-export const dumpArguments = (...args) => args.map(arg => toString.call(arg).slice(8, -1)).join(', ');
-
-export function getFuncCode(fn) {
-    if (typeof fn=='function') {
-        let str = Function.prototype.toString.call(fn),
-            rawCode = str[str.length - 1] === '}' ? str.slice(str.indexOf("{") + 1, -1) : str.slice(str.indexOf('=>') + 2),
-            code= rawCode.trim().replace(/\s{2,}/g, ' ');
-
-        return code === '[native code]'? null : code;
-    }
-
-    return null;
-}
